946c2254ef6619c51c8b45371acf6c0a74decc37
Kyle Liebler <kyle@airspace-intelligence.com>
2024-08-08 23:10:45 -0400

    refactor

--- summarize/__main__.py
+++ summarize/__main__.py
@@ -1,144 +1,135 @@
 import os
-
-from summarize.settings import OPENAI_API_KEY
-from langchain_community.document_loaders import PyPDFLoader
-
-loader = PyPDFLoader("test.pdf")
-pages = loader.load_and_split()
-text = ' '.join([page.page_content.replace('\t', ' ') for page in pages])
-
 import re
-def clean_text(text):
-   # Remove the specific phrase 'Free eBooks at Planet eBook.com' and surrounding whitespace
-   cleaned_text = re.sub(r'\s*Free eBooks at Planet eBook\.com\s*', '', text, flags=re.DOTALL)
-   # Remove extra spaces
-   cleaned_text = re.sub(r' +', ' ', cleaned_text)
-   # Remove non-printable characters, optionally preceded by 'David Copperfield'
-   cleaned_text = re.sub(r'(David Copperfield )?[\x00-\x1F]', '', cleaned_text)
-   # Replace newline characters with spaces
-   cleaned_text = cleaned_text.replace('\n', ' ')
-   # Remove spaces around hyphens
-   cleaned_text = re.sub(r'\s*-\s*', '', cleaned_text)
-   return cleaned_text
-clean_text=clean_text(text)
+import numpy as np
+import pandas as pd
+import openai
+import faiss
+from fpdf import FPDF
+from tqdm import tqdm
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_openai import ChatOpenAI
-llm = ChatOpenAI(model="gpt-4o")
-Tokens = llm.get_num_tokens(clean_text)
-print (f"We have {Tokens} tokens in the book")
-
 from langchain_experimental.text_splitter import SemanticChunker
 from langchain_openai.embeddings import OpenAIEmbeddings
-text_splitter = SemanticChunker(
-   OpenAIEmbeddings(), breakpoint_threshold_type="interquartile"
-)
-docs = text_splitter.create_documents([clean_text])
-import numpy as np
-import openai
-def get_embeddings(text):
-   response = openai.embeddings.create(
-       model="text-embedding-3-small",
-       input=text
-   )
-   return response.data
-embeddings=get_embeddings([doc.page_content for doc in docs]
-)
-import pandas as pd
-content_list = [doc.page_content for doc in docs]
-df = pd.DataFrame(content_list, columns=['page_content'])
-vectors = [embedding.embedding for embedding in embeddings]
-array = np.array(vectors)
-embeddings_series = pd.Series(list(array))
-df['embeddings'] = embeddings_series
-
-
-import numpy as np
-import faiss
-
-# Convert to float32 if not already
-array = array.astype('float32')
-
-num_clusters = 50 if len(array) > 50 else len(array)
-
-# Vectors dimensionality
-dimension = array.shape[1]
-
-# Train KMeans with Faiss
-kmeans = faiss.Kmeans(dimension, num_clusters, niter=20, verbose=True)
-kmeans.train(array)
-
-# Directly access the centroids
-centroids = kmeans.centroids
-
-# Create a new index for the original dataset
-index = faiss.IndexFlatL2(dimension)
-
-# Add original dataset to the index
-index.add(array)
-
-D, I = index.search(centroids, 1)
-sorted_array = np.sort(I, axis=0)
-sorted_array=sorted_array.flatten()
-extracted_docs = [docs[i] for i in sorted_array]
-model = ChatOpenAI(temperature=0,model="gpt-4")
 from langchain_core.output_parsers import StrOutputParser
-from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
-prompt = ChatPromptTemplate.from_template("""
-You will be given different passages from a book one by one. Provide a summary of the following text. Your result must be detailed and atleast 2 paragraphs. When summarizing, directly dive into the narrative or descriptions from the text without using introductory phrases like 'In this passage'. Directly address the main events, characters, and themes, encapsulating the essence and significant details from the text in a flowing narrative. The goal is to present a unified view of the content, continuing the story seamlessly as if the passage naturally progresses into the summary
+from summarize.settings import OPENAI_API_KEY
 
-Passage:
 
-```{text}```
-SUMMARY:
-"""
-)
-chain= (
-    prompt
-   | model
-   |StrOutputParser() )
-from tqdm import tqdm
-final_summary = ""
+class PDFSummaryGenerator:
+    def __init__(self, pdf_path, api_key):
+        self.loader = PyPDFLoader(pdf_path)
+        self.pages = self.loader.load_and_split()
+        self.api_key = api_key
 
-for doc in tqdm(extracted_docs, desc="Processing documents"):
-   # Get the new summary.
-   new_summary = chain.invoke({"text": doc.page_content})
-   # Update the list of the last two summaries: remove the first one and add the new one at the end.
-   final_summary+=new_summary
+    def _clean_text(self, text):
+        text = re.sub(
+            r"\s*Free eBooks at Planet eBook\.com\s*", "", text, flags=re.DOTALL
+        )
+        text = re.sub(r" +", " ", text)
+        text = re.sub(r"(David Copperfield )?[\x00-\x1F]", "", text)
+        text = text.replace("\n", " ")
+        text = re.sub(r"\s*-\s*", "", text)
+        return text
 
-last_summary = final_summary
-from fpdf import FPDF
+    def _generate_clean_text(self):
+        return " ".join(
+            [
+                self._clean_text(page.page_content.replace("\t", " "))
+                for page in self.pages
+            ]
+        )
 
-class PDF(FPDF):
-   def header(self):
-       # Select Arial bold 15
-       self.set_font('Arial', 'B', 15)
-       # Move to the right
-       self.cell(80)
-       # Framed title
-       self.cell(30, 10, 'Summary', 1, 0, 'C')
-       # Line break
-       self.ln(20)
-   
-   def footer(self):
-       # Go to 1.5 cm from bottom
-       self.set_y(-15)
-       # Select Arial italic 8
-       self.set_font('Arial', 'I', 8)
-       # Page number
-       self.cell(0, 10, 'Page %s' % self.page_no(), 0, 0, 'C')
+    def _get_embeddings(self, texts):
+        response = openai.embeddings.create(model="text-embedding-3-small", input=texts)
+        return response.data
 
-# Instantiate PDF object and add a page
-pdf = PDF()
-pdf.add_page()
-pdf.set_font("Arial", size=12)
+    def _create_embeddings_dataframe(self, docs):
+        content_list = [doc.page_content for doc in docs]
+        df = pd.DataFrame(content_list, columns=["page_content"])
+        vectors = [
+            embedding.embedding for embedding in self._get_embeddings(content_list)
+        ]
+        df["embeddings"] = pd.Series(list(np.array(vectors)))
+        return df
 
-# Ensure the 'last_summary' text is treated as UTF-8
-# Replace 'last_summary' with your actual text variable if different
-# Make sure your text is a utf-8 encoded string
-last_summary_utf8 = last_summary.encode('latin-1', 'replace').decode('latin-1')
-pdf.multi_cell(0, 10, last_summary_utf8)
+    def _train_kmeans(self, array):
+        array = array.astype("float32")
+        num_clusters = 50 if len(array) > 50 else len(array)
+        dimension = array.shape[1]
+        kmeans = faiss.Kmeans(dimension, num_clusters, niter=20, verbose=True)
+        kmeans.train(array)
+        return kmeans.centroids, dimension
 
-# Save the PDF to a file
-pdf_output_path = "summary.pdf"
-pdf.output(pdf_output_path)
+    def _index_and_sort_documents(self, centroids, dimension, array, docs):
+        index = faiss.IndexFlatL2(dimension)
+        index.add(array)
+        D, I = index.search(centroids, 1)
+        sorted_indices = np.sort(I, axis=0).flatten()
+        return [docs[i] for i in sorted_indices]
 
+    def _generate_summary(self, docs):
+        model = ChatOpenAI(temperature=0, model="gpt-4")
+        prompt = ChatPromptTemplate.from_template(
+            """
+            You will be given different passages from a book one by one. Provide a summary of the following text. 
+            Your result must be detailed and at least 2 paragraphs. When summarizing, directly dive into the narrative 
+            or descriptions from the text without using introductory phrases like 'In this passage'. Directly address 
+            the main events, characters, and themes, encapsulating the essence and significant details from the text 
+            in a flowing narrative. The goal is to present a unified view of the content, continuing the story seamlessly 
+            as if the passage naturally progresses into the summary.
+
+            Passage:
+
+            ```{text}```
+            SUMMARY:
+        """
+        )
+        chain = prompt | model | StrOutputParser()
+        final_summary = ""
+        for doc in tqdm(docs, desc="Processing documents"):
+            final_summary += chain.invoke({"text": doc.page_content})
+        return final_summary
+
+    class PDF(FPDF):
+        def header(self):
+            self.set_font("Arial", "B", 15)
+            self.cell(80)
+            self.cell(30, 10, "Summary", 1, 0, "C")
+            self.ln(20)
+
+        def footer(self):
+            self.set_y(-15)
+            self.set_font("Arial", "I", 8)
+            self.cell(0, 10, f"Page {self.page_no()}", 0, 0, "C")
+
+    def generate_pdf_summary(self, output_file_name="summary.pdf"):
+        clean_text = self._generate_clean_text()
+
+        llm = ChatOpenAI(model="gpt-4o")
+        tokens = llm.get_num_tokens(clean_text)
+        print(f"We have {tokens} tokens in the book")
+
+        text_splitter = SemanticChunker(
+            OpenAIEmbeddings(), breakpoint_threshold_type="interquartile"
+        )
+        docs = text_splitter.create_documents([clean_text])
+
+        df = self._create_embeddings_dataframe(docs)
+        array = np.vstack(df["embeddings"])
+
+        centroids, dimension = self._train_kmeans(array)
+        sorted_docs = self._index_and_sort_documents(centroids, dimension, array, docs)
+
+        last_summary = self._generate_summary(sorted_docs)
+
+        pdf = self.PDF()
+        pdf.add_page()
+        pdf.set_font("Arial", size=12)
+        last_summary_utf8 = last_summary.encode("latin-1", "replace").decode("latin-1")
+        pdf.multi_cell(0, 10, last_summary_utf8)
+        pdf.output(output_file_name)
+
+
+if __name__ == "__main__":
+    generator = PDFSummaryGenerator(pdf_path="test.pdf", api_key=OPENAI_API_KEY)
+    generator.generate_pdf_summary("summary.pdf")
